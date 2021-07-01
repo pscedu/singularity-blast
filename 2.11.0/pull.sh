15f99ed21b790a42edea6a87a2567db1836e5d85
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-30 20:22:29 -0400

    Delete pull.sh

--- 2.11.0/pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! $(command -v singularity) ]; then
-	module load singularity
-fi
-
-singularity pull https://depot.galaxyproject.org/singularity/blast%3A2.9.0--pl526he19e7b1_5
-mv -v blast%3A2.9.0--pl526he19e7b1_5 singularity-BLAST-2.9.0.sif
